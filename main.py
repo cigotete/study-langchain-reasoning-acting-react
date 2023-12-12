43ca4289711c96260b897b478d31509cc9be4af7
Camilo Figueroa <cigotete@gmail.com>
2023-12-12 17:21:58 -0500

    Implementing intermediate steps.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.schema import AgentAction, AgentFinish
 from langchain.tools import Tool
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.agents.format_scratchpad import format_log_to_str
 
 
 @tool
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     Begin!
     
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
 
     prompt = PromptTemplate.from_template(template=template).partial(
@@ -58,6 +59,8 @@ if __name__ == "__main__":
     llm = ChatOpenAI(
         temperature=0, model_kwargs={'stop':["\nObservation"]}
     )
+    intermediate_steps = []
+
     # Agent variable is defined using a chain of three functions using the | operator.
     # Anonymous function: { "input": lambda x: x["input"] }
     # takes a dictionary as input and returns a new dictionary with the same content.
@@ -68,6 +71,7 @@ if __name__ == "__main__":
     agent = (
         {
             "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
         }
         | prompt
         | llm
@@ -79,6 +83,7 @@ if __name__ == "__main__":
     agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
         {
             "input": "What is the length of characters of the following text: DOG?",
+            "agent_scratchpad": intermediate_steps,
         }
     )
     print(agent_step)
@@ -90,3 +95,14 @@ if __name__ == "__main__":
         tool_input = agent_step.tool_input # Get tool input
         observation = tool_to_use.func(str(tool_input)) # Run tool and get observation
         print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation))) # Add observation to intermediate steps
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of characters of the following text: DOG?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
