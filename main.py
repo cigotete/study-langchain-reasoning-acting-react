4702d6482e0c8cccfccad75b0c05c7f1ecf264de
Camilo Figueroa <cigotete@gmail.com>
2023-12-12 16:41:04 -0500

    Lines related with execution of agent action and agent finish.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+from typing import Union, List
 
 from langchain.agents import tool
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import Tool
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 
@@ -12,6 +15,13 @@ def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
     print(f"get_text_length enter with {text=}")
     return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")
 
 
 if __name__ == "__main__":
@@ -64,5 +74,19 @@ if __name__ == "__main__":
         | ReActSingleInputOutputParser()
     )
 
-    res = agent.invoke({"input": "What is the length of 'DOG' in characters?"})
-    print(res)
+    # The invoke method of the agent is called with a dictionary containing the input question.
+    # This call returns an object of type AgentAction or AgentFinish.
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of characters of the following text: DOG?",
+        }
+    )
+    print(agent_step)
+
+    # Checks if the agent_step is an AgentAction object. If it is, steps are performed.
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool # Get tool name
+        tool_to_use = find_tool_by_name(tools, tool_name) # Find tool by name
+        tool_input = agent_step.tool_input # Get tool input
+        observation = tool_to_use.func(str(tool_input)) # Run tool and get observation
+        print(f"{observation=}")
